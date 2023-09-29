c1c0311fb5be2a3ede0dc853154e7e9354976aae
tyrad <tyrad@TDP>
2023-09-29 10:59:29 -0400

    Add project files.

---
+++ EnumPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyFileClass file1 = new MyFileClass("picture.txt", @"C:\users\tyrad\Documents", 
+                Rights.READABLE | Rights.WRITABLE,
+                null);
+
+            Console.WriteLine(file1.FileRights);
+            //Create an enum use or |
+            //To Check an enum use and &
+            //Make the lab public
+        }
+    }
+}
---
+++ EnumPractice/Rights.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumPractice
+{
+    [Flags]
+    internal enum Rights
+    {
+        NONE = 0,
+        READABLE = 1,
+        WRITABLE = 2,
+        EXUCUTABLE = 4,
+        PRIVILEGED = 8
+
+
+    }
+}
